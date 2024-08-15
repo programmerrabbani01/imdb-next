faddf556b563f8dc8280389f1b9c9aa1869d27ba
programmerrabbani01 <programmerrabbani@gmail.com>
2024-08-15 19:33:36 +0600

    Dark Mode Problem Fix Done

--- components/DarkMode/DarkModeSwitch.tsx
+++ components/DarkMode/DarkModeSwitch.tsx
@@ -1,11 +1,20 @@
 "use client";
 
-import { MdLightMode, MdDarkMode } from "react-icons/md";
-
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function DarkModeSwitch() {
   const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Ensure that we are on the client side before rendering
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // If not mounted, return null to prevent rendering
+  if (!mounted) return null;
 
   const currentTheme = theme === "system" ? systemTheme : theme;
 
@@ -18,7 +27,7 @@ export default function DarkModeSwitch() {
         />
       ) : (
         <MdDarkMode
-          className="text-gray-800 hover:text-amber-500  transition cursor-pointer text-xl"
+          className="text-gray-800 hover:text-amber-500 transition cursor-pointer text-xl"
           onClick={() => setTheme("dark")}
         />
       )}
